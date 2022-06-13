6f561b45e64c70a296f8bd93868d8b9a371f0cb8
77den <77denisov@gmail.com>
2022-06-13 12:29:47 +0300

    Init commit Single activity Fragment plus viewModel

---
+++ app/src/main/java/ru/dw/to_dolist/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.dw.to_dolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.dw.to_dolist.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ru/dw/to_dolist/ui/main/MainFragment.kt
@@ -0,0 +1,45 @@
+package ru.dw.to_dolist.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.dw.to_dolist.R
+import ru.dw.to_dolist.databinding.ActivityMainBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
